c55db361f27c09840b95947befc8d942edd27577
dzambranob <dzambranob@unal.edu.co>
2022-10-23 23:34:11 -0500

    Added topbar and viewProfile, editProfile left

--- App.js
+++ App.js
@@ -1,29 +1,31 @@
-import { View, Text, StyleSheet } from 'react-native'
-//import { ApolloClient } from 'apollo-boost';
+import { View, Text, StyleSheet, Image, StatusBar } from 'react-native'
 import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
-import HomePage from './src/HomePage'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomePage from './src/screens/HomePage';
+import Profile from './src/screens/Profile';
+import EditProfile from './src/screens/EditProfile';
 
 const client = new ApolloClient({
-  uri: 'http://192.168.0.7:5000/graphql',  //Recordar que esto debe cambiar!!!!!!!!!!!!
+  uri: 'http://192.168.0.4:5000/graphql',  //Recordar que esto debe cambiar!!!!!!!!!!!!
   cache: new InMemoryCache()
-})
+});
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
-       <View style={styles.container}>
-      <Text style={styles.title}>Tituloooooo</Text>
-      <HomePage/> 
-    </View>
+      <NavigationContainer>
+        <StatusBar hidden={true}/>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name = "HomePage" component={HomePage} options={{headerShown:false}}/>
+          <Stack.Screen name = "Profile" component={Profile} options={{headerShown:false}}/>
+          <Stack.Screen name = "EditProfile" component={EditProfile} options={{headerShown:false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </ApolloProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+/*  */
--- src/gql/Query.js
+++ src/gql/Query.js
@@ -7,3 +7,49 @@ query searchAllSubjects {
     }
   }
 `;
+
+export const viewProfile = gql `
+query viewProfile($username : String!) {
+  viewProfile(username:$username){
+    data{
+      UserName
+	    UserType 
+	    FullName
+	    Document
+	    Email
+	    DepDocument
+	    CityDocument
+	    Genre
+	    Cel
+	    Tel
+	    Age
+	    BirthPlace
+	    Country
+	    BloodType
+	    Address
+	    ArmyCard
+	    MotherFullName
+	    MotherDocument
+	    FatherFullName
+	    FatherDocument
+    }
+  }
+}
+`
+
+export const updateProfile = gql `
+mutation updateProfile($username:String!, $Email:String!, $Cel:Float!, $Tel:Float!, $Address:String!){
+  updateProfile(username:"dzambranob", profile:{
+    Email:$Email
+    Cel:$Cel
+    Tel:$Tel
+    Address:$Address
+  }){
+    data{
+      UserName
+      UNMail
+      Cel
+    }
+  }
+}
+`
